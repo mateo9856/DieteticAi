e7d5ca284dd277ff01540c52f729eb80c91169ae
Mateusz M <mateo9656@gmail.com>
2026-03-18 18:54:21 +0100

    Prepare logon inputs

--- DieteticAI.UI/Tools/SessionManager.cs
+++ DieteticAI.UI/Tools/SessionManager.cs
@@ -2,11 +2,7 @@ namespace DieteticAI.UI.Tools;
 
 public class SessionManager
 {
-    public string? UserId
-    {
-        get;
-        set => field = value;
-    }
+    public string? UserId { get; set; }
 
     public bool? IsActiveRabbitConnection { get; set; }
     
