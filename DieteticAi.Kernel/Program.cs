6c0adacd96307dd2510564398c45a1a590a51fd9
Mateusz M <mateo9656@gmail.com>
2026-05-16 10:20:28 +0200

    Code fixes

--- DieteticAi.Kernel/Program.cs
+++ DieteticAi.Kernel/Program.cs
@@ -6,11 +6,15 @@ using DietAI.RabbitServer.Abstractions.RabbitConnection;
 using DietAI.RabbitServer.Implementations.RabbitConnection;
 using DietAI.RabbitServer.Implementations.ReceiverService;
 using DietAI.RabbitServer.Implementations.SenderService;
+using DieteticAi.Models;
+using DieteticAi.Plugins;
 using DieteticAi.Tools;
+using DieteticAi.Tools.Wrappers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
 
 namespace DieteticAi;
 
@@ -26,19 +30,31 @@ class Program
         var kernelConfig = new KernelConfiguration();
         configuration.Bind(kernelConfig);
         
-        var aiConfig = new AiModelSelector(kernelConfig);
-        var kernel = aiConfig.BuildKernel();
-        
-        if (kernelConfig.TestMode)
-        {
-            await new DietSimulator(kernel).Run();
-            return;
-        }
-
-        var host = Host.CreateDefaultBuilder(args)
+        var hostBuilder = Host.CreateDefaultBuilder(args)
             .ConfigureServices(services =>
             {
                 services.AddSingleton<IConfiguration>(configuration);
+                services.AddSingleton(kernelConfig);
+
+                services.AddSingleton(serviceProvider =>
+                {
+                    var aiConfig = new AiModelSelector(serviceProvider.GetRequiredService<KernelConfiguration>());
+                    return aiConfig.BuildKernel();
+                });
+                services.AddSingleton<IKernelWrapper, KernelWrapper>();
+                services.AddSingleton<IList<Diets>>(_ => new List<Diets>());
+                services.AddSingleton(serviceProvider =>
+                {
+                    var dietPlugin = new DietPlugin(
+                        serviceProvider.GetRequiredService<IList<Diets>>(),
+                        serviceProvider.GetRequiredService<IKernelWrapper>());
+
+                    var kernel = serviceProvider.GetRequiredService<Microsoft.SemanticKernel.Kernel>();
+                    kernel.Plugins.AddFromObject(dietPlugin, nameof(DietPlugin));
+
+                    return dietPlugin;
+                });
+                services.AddSingleton<DietSimulator>();
 
                 services.AddSingleton<IRabbitConnectionFactory, RabbitConnectionFactory>();
                 services.AddSingleton<ITopicFactory, TopicFactory>();
@@ -47,7 +63,7 @@ class Program
                 services.AddTransient<IReceiveService, ReceiverService>();
                 services.AddTransient<ISenderService, SenderService>();
 
-                services.AddTransient(serviceProvider => new DietService(kernel));
+                services.AddTransient<DietService>();
 
                 services.AddHostedService<DietConcurrentRunner>();
 
@@ -55,8 +71,16 @@ class Program
                 {
                     builder.AddEventSourceLogger();
                 });
-            })
-            .Build();
+            });
+
+        if (kernelConfig.TestMode)
+        {
+            using var testHost = hostBuilder.Build();
+            await testHost.Services.GetRequiredService<DietSimulator>().Run();
+            return;
+        }
+
+        var host = hostBuilder.Build();
 
         await host.RunAsync();
     }    
